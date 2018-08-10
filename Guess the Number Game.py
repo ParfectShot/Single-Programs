92b4d0bae7eea96f8de25449d8f152427e402230
ParfectShot <38669242+ParfectShot@users.noreply.github.com>
2018-08-11 02:43:09 +0530

    1st Program
    Just a simple game of guessing the number.

---
+++ Guess the Number Game.py
@@ -0,0 +1,32 @@
+# This is a Guess the Number Game
+
+import random
+
+secret=random.randint(1,21)
+
+#Asking the User to enter the no.
+
+print("I am thinking of a number between 1-20")
+print("Take a Guess..(Only Six Chances)")
+
+
+
+for i in range(1,7):
+    number = int(input())
+
+    if number<secret:
+        print("A little high")
+
+
+    elif number>secret:
+        print("A little low")
+
+
+    else:
+        break
+
+if number==secret:
+    print("You guessed it in " + str(i) + " guesses")
+    print("Number is : "+str(secret))
+else:
+    print("Better Luck next time :)")
